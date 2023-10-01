895ffc55a227c750600eedc2a8a48148cf0f41e0
ROHAN4535 <grohan468@gmail.com>
2023-10-01 20:06:07 +0530

    Context API

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
 
@@ -7,9 +6,8 @@ const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <Navigation onLogout={props.onLogout} />
     </header>
   );
 };
-
 export default MainHeader;
